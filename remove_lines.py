832b8d708ea4be97ae2674ec1e31d38a97ba3a55
Gabriel Lavoura <lavourasantos@gmail.com>
2025-10-20 20:06:44 -0300

    chore: normalization and binarization

--- remove_lines.py
+++ remove_lines.py
@@ -12,3 +12,13 @@ img = cv2.imread(inp, cv2.IMREAD_GRAYSCALE)
 if img is None:
     print(f"Erro: não foi possível ler {inp}")
     sys.exit(1)
+
+img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX)
+_, binary = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+h_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (80, 2))
+v_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 80))
+
+detect_h = cv2.morphologyEx(binary, cv2.MORPH_OPEN, h_kernel)
+detect_v = cv2.morphologyEx(binary, cv2.MORPH_OPEN, v_kernel)
+lines = cv2.bitwise_or(detect_h, detect_v)
