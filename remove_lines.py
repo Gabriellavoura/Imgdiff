2bb2fca704c5596b3200a4fc6a8a4bf18bb2be3b
Gabriel Lavoura <lavourasantos@gmail.com>
2025-10-20 20:06:08 -0300

    chore: read with grayscale

--- remove_lines.py
+++ remove_lines.py
@@ -1,3 +1,14 @@
 import cv2
 import numpy as np
 import sys, os
+
+
+if len(sys.argv) < 3:
+    print("Uso: remove_lines.py <input> <output>")
+    sys.exit(1)
+
+inp, out = sys.argv[1], sys.argv[2]
+img = cv2.imread(inp, cv2.IMREAD_GRAYSCALE)
+if img is None:
+    print(f"Erro: não foi possível ler {inp}")
+    sys.exit(1)
